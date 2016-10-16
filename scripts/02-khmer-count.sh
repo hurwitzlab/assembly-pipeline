6c94e61b31a6cb42b05345e9f5f733345b011c67
Scott G. Daniel <scottdaniel25@gmail.com>
2016-10-16 09:09:02 -0700

    the next step

---
+++ scripts/02-khmer-count.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#
+# Merge *.fastq's by sample before deduplication 
+#
+
+set -u
+source ./config.sh
+export CWD="$PWD"
+export STEP_SIZE=1
+
+PROG=`basename $0 ".sh"`
+STDOUT_DIR="$CWD/out/$PROG"
+
+init_dir "$STDOUT_DIR"
+#init_dir "$DEDUPED_DIR"
+
+cd "$GROUPED_DIR"
+
+#overriding config just this once
+export SAMPLE_NAMES="DNA_3"
+
+for i in $SAMPLE_NAMES; do
+    export SAMPLE=$i
+    echo $i
+    qsub -V -j oe -o "$STDOUT_DIR" $WORKER_DIR/dedup.sh
+done
+
---
+++ scripts/workers/count-kmers.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+#PBS -W group_list=bhurwitz
+#PBS -q qualified
+#PBS -l select=1:ncpus=12:mem=96gb
+###and the amount of time required to run it
+#PBS -l walltime=2:00:00
+#PBS -l cput=2:00:00
+#PBS -M scottdaniel@email.arizona.edu
+#PBS -m bea
+
+echo Started at $(date)
+
+if [ -n "$PBS_O_WORKDIR" ]; then
+    cd $PBS_O_WORKDIR
+fi
+
+set -u
+
+COMMON="$WORKER_DIR/common.sh"
+
+if [ -e $COMMON ]; then
+  . "$COMMON"
+else
+  echo Missing common \"$COMMON\"
+  exit 1
+fi
+
+cd $GROUPED_DIR
+
+#export MERGER="$WORKER_DIR/mergeShuffledFastqSeqs.pl"
+
+echo Doing sample $SAMPLE
+
+#set -x
+
+#echo Concatenating all fastqs into temp file
+#time cat "$SAMPLE".1.fastq "$SAMPLE".2.fastq "$SAMPLE".nomatch.fastq > "$SAMPLE".temp
+echo Filtering out duplicate reads
+time fastq filter --adjust 64 --unique "$SAMPLE".temp \
+    > $DEDUPED_DIR/"$SAMPLE".deduped.fastq
+if [[ $? -eq 0 ]]; then
+    echo Removing temp file
+    rm "$SAMPLE".temp
+else
+    echo Something went wrong with fastq filter
+    echo Keeping temp file for another try
+fi
+
+echo Done at $(date)
