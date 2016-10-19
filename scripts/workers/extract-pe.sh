181a09d9cc4370e79f20e180f411e32b4d4f6784
Scott G. Daniel <scottdaniel25@gmail.com>
2016-10-18 20:13:02 -0700

    added fastq-sort to extract-pe

--- scripts/workers/extract-pe.sh
+++ scripts/workers/extract-pe.sh
@@ -6,8 +6,8 @@
 #PBS -l select=1:ncpus=2:mem=3gb:pcmem=2gb
 #PBS -l pvmem=6gb
 ###and the amount of time required to run it
-#PBS -l walltime=6:00:00
-#PBS -l cput=6:00:00
+#PBS -l walltime=24:00:00
+#PBS -l cput=24:00:00
 #PBS -M scottdaniel@email.arizona.edu
 #PBS -m bea
 
@@ -33,10 +33,27 @@ cd $TRIMMED_DIR
 echo Doing sample $SAMPLE
 
 set -x
+echo sorting pe
+time fastq-sort --id "$SAMPLE".deduped.fastq.abundfilt > "$SAMPLE".temp
+
+if [[ $? -eq 0 ]]; then
+    echo "..."
+else
+    echo Something went wrong with fastq-sort
+    exit 1
+fi
+
+
 echo extracting pe
 time extract-paired-reads.py --gzip \
     --output-paired $READY_DIR/"$SAMPLE".fastq.pe.gz \
     --output-single $READY_DIR/"$SAMPLE".fastq.se.gz \
-    "$SAMPLE".deduped.fastq.abundfilt
+    "$SAMPLE".temp
+
+if [[ $? -eq 0 ]]; then
+    rm "$SAMPLE".temp
+else
+    echo Something went wrong with extract-paired-reads.py
+fi
 
 echo Done at $(date)
