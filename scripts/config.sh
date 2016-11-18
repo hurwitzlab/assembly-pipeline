24d78b36ceaed2a3c4bed1e0a11bdec7fca6a92e
Scott G. Daniel <scottdaniel25@gmail.com>
2016-11-18 14:31:04 -0700

    dirs to setups

--- scripts/config.sh
+++ scripts/config.sh
@@ -3,36 +3,38 @@ export SCRIPT_DIR=$CWD
 export WORKER_DIR="$SCRIPT_DIR/workers"
 #where programs are
 export BIN_DIR="/rsgrps/bhurwitz/hurwitzlab/bin"
-#root project dir
-export PRJ_DIR="/rsgrps/bhurwitz/scottdaniel/assembly-pipeline"
-
+#assemblies of ALL the reads
+export ALL_DIR="/rsgrps/bhurwitz/scottdaniel/assembly-pipeline"
+#project dir
+export PRJ_DIR="/rsgrps/bhurwitz/scottdaniel/unknown-assembly"
+#source dir for fastqs
+export SRC_DIR="/rsgrps/bhurwitz/scottdaniel/fastq-taxoner-patric"
 # 
 # Sample names
 #
 export SAMPLE_NAMES="DNA_1 DNA_2 DNA_3 DNA_4"
 
+#place for sorted and merged PE'd reads and orphaned single reads (if we need them)
+export SORTNMG_DIR="$SRC_DIR/sort-and-merged"
+#lq-reads
+export LQ_DIR="$SRC_DIR/lq-out"
+#unknown species reads
+export UNK_DIR="$SRC_DIR/unk-out2"
 
-#place for sorted and merged PE'd reads and orphaned single reads
-export SORTNMG_DIR="/rsgrps/bhurwitz/scottdaniel/fastq-taxoner-patric/sort-and-merged"
-
-#place to store merged fastq's by sample
-export GROUPED_DIR="$PRJ_DIR/grouped"
-#place to store deduped fastq's by sample
-#export DEDUPED_DIR="$PRJ_DIR/deduped"
-export DEDUPED_DIR="$PRJ_DIR/concat"
+#place to store cat'd fastqs 
+export GROUPED_DIR="$PRJ_DIR/concat"
+#place to store deduped fastqs
+export DEDUPED_DIR="$PRJ_DIR/deduped"
 #will store graphs in $DEDUPED_DIR
 
-#place to store read trimmed of low-abundance kmers
-#export TRIMMED_DIR="$PRJ_DIR/trimmed"
-export TRIMMED_DIR="$PRJ_DIR/concat-trimmed"
-#place to store gzipped paired-end / single-end reads that are ready for assembly
-#export READY_DIR="$PRJ_DIR/ready"
-export READY_DIR="$PRJ_DIR/concat-ready"
+#place for fastqs ready to assembly
+export READY_DIR="$PRJ_DIR/ready"
 #place to store assembled contigs 
 export CONTIG_DIR="$PRJ_DIR/contigs"
-#export CONTIG_DIR="$PRJ_DIR/concat-contigs"
+
 #place to store quality reports from quast
-export QUAST_DIR="$PRJ_DIR/quast_results"
+#will use old "assembly-pipeline" dir because that will allows us to compare
+export QUAST_DIR="$SRC_DIR/quast_results"
 #place to store alignment of original reads to contigs for calculation of depth
 export ALIGN_DIR="$PRJ_DIR/aligned"
 
